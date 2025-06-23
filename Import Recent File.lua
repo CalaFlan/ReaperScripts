190f4a680a0424af66c2b738410cf9fe97d55072
Callum Flanagan <100199903+CalaFlan@users.noreply.github.com>
2025-06-23 17:39:15 +0100

    Create Import Recent File.lua
    A script for quickly importing the most recent file in a specified folder into reaper

---
+++ Import Recent File.lua
@@ -0,0 +1,17 @@
+local function get_last_file_name(directory)
+    local command = 'dir /a-d /o-d /tc /b "'..directory..'" 2>nul:'
+    -- /tw for last modified file
+    -- /tc for last created file
+    local pipe = io.popen(command)
+    local file_name = pipe:read()
+    pipe:close()
+    return file_name
+ end
+ 
+ local directory = [[d:\OBSRecordings\]] -- <--- Replace this with your chosen directory (OBS Output path, Downloads, Ect)
+ local file_name = get_last_file_name(directory)
+ if file_name then
+    reaper.InsertMedia(directory..file_name,1)
+ else
+    reaper.ShowConsoleMsg("Directory is empty")
+ end
